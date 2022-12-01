c2e2be2ab69e7cbbef2a90df056dab87d7742fc1
Karlos Zurutuza <55366904+torfoes@users.noreply.github.com>
2022-12-01 16:44:43 -0600

    Getting started!

---
+++ BloonEntity.py
@@ -0,0 +1,7 @@
+class BloonEntity:
+    def __init__(self, bloon_type, num_bloons, cost, eco_boost, bloon_delay):
+        self.bloon_type = bloon_type
+        self.num_bloons = num_bloons
+        self.cost = cost
+        self.eco_boost = eco_boost
+        self.bloon_delay = bloon_delay
--- main.py
+++ main.py
@@ -1,30 +1,20 @@
-import csv
-
-with open('bloon_data.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in reader:
-        print(', '.join(row))
-
-
-class BloonEntity:
-    def __init__(self, a, b):
-        self.__a = a
-        self.__b = b
-
-    def get_a(self):
-        return self.__a
-
-    def get_b(self):
-        return self.__b
+from pandas import read_csv
+import BloonEntity
 
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def run():
+    print(bloon_data.values)
+
+
+def __init__():
+    global bloon_data
+    bloon_data = read_csv('bloon_data.csv')
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    run()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
